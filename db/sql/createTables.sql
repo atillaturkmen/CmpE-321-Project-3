914352ea8cf10313fb3001807687c542aa0d9eb9
atillaturkmen <turkmenatilla522@gmail.com>
2022-04-19 15:47:03 +0300

    connect to mysql db

---
+++ db/sql/createTables.sql
@@ -0,0 +1,92 @@
+-- Atilla Türkmen, Onur Kömürcü
+-- Creates tables for 2. project of CmpE 321 course
+-- Written for MYSQL
+
+-- we couldn't limit row number in this table (limit should be 4)
+CREATE TABLE `Database_Manager` (
+  `username` VARCHAR(250) NOT NULL, -- 250 is greatest varchar length that is indexable
+  `password` VARCHAR(250) NOT NULL,
+  PRIMARY KEY (`username`)
+);
+
+CREATE TABLE `Department` (
+  `department_id` VARCHAR(250) NOT NULL,
+  `name` VARCHAR(250) NOT NULL UNIQUE,
+  PRIMARY KEY (`department_id`)
+);
+
+CREATE TABLE `User` (
+  `username` VARCHAR(250) NOT NULL,
+  `password` VARCHAR(250) NOT NULL,
+  `name` VARCHAR(250) NOT NULL,
+  `surname` VARCHAR(250) NOT NULL,
+  `email` VARCHAR(250) NOT NULL,
+  `department_id` VARCHAR(250) NOT NULL,
+  PRIMARY KEY (`username`),
+  FOREIGN KEY (`department_id`)
+  REFERENCES `Department` (`department_id`)
+);
+
+CREATE TABLE `Instructor` (
+  `username` VARCHAR(250) NOT NULL,
+  `title` VARCHAR(250) NOT NULL,
+  PRIMARY KEY (`username`),
+  FOREIGN KEY (`username`)
+  REFERENCES `User` (`username`)
+);
+
+CREATE TABLE `Student` (
+  `username` VARCHAR(250) NOT NULL,
+  `student_id` VARCHAR(250) NOT NULL UNIQUE,
+  PRIMARY KEY (`username`),
+  FOREIGN KEY (`username`)
+  REFERENCES `User` (`username`)
+);
+
+CREATE TABLE `Classroom` (
+  `classroom_id` VARCHAR(200) NOT NULL,
+  `campus` VARCHAR(250) NOT NULL,
+  `capacity` INT NOT NULL,
+  PRIMARY KEY (`classroom_id`)
+);
+
+CREATE TABLE `Course` (
+  `course_id` VARCHAR(250) NOT NULL,
+  `name` VARCHAR(250) NOT NULL,
+  `credits` INT NOT NULL,
+  `quota` INT NOT NULL,
+  `slot` INT NOT NULL,
+  `classroom_id` VARCHAR(200) NOT NULL, -- 200 instead of 250 for composite unique index
+  `instructor_username` VARCHAR(250) NOT NULL,
+  PRIMARY KEY (`course_id`),
+  FOREIGN KEY (`classroom_id`)
+  REFERENCES `Classroom` (`classroom_id`),
+  FOREIGN KEY (`instructor_username`)
+  REFERENCES `Instructor` (`username`),
+  UNIQUE KEY dontOverlap (`slot`,`classroom_id`),
+  CHECK (slot>=1), -- slot can be from monday to friday
+  CHECK (slot<=10)
+);
+
+-- if grade is null, that means student is taking the course this semester
+CREATE TABLE `Grades` (
+  `student_id` VARCHAR(250) NOT NULL,
+  `course_id` VARCHAR(250) NOT NULL,
+  `grade` FLOAT,
+  PRIMARY KEY (`student_id`, `course_id`),
+  FOREIGN KEY (`student_id`)
+  REFERENCES `Student` (`student_id`),
+  FOREIGN KEY (`course_id`)
+  REFERENCES `Course` (`course_id`)
+);
+
+CREATE TABLE `Prerequisites` (
+  `prq_for` VARCHAR(250) NOT NULL,
+  `prq` VARCHAR(250) NOT NULL,
+  PRIMARY KEY (`prq_for`, `prq`),
+  FOREIGN KEY (`prq_for`)
+  REFERENCES `Course` (`course_id`),
+  FOREIGN KEY (`prq`)
+  REFERENCES `Course` (`course_id`),
+  CHECK (STRCMP(prq_for, prq)=1)
+);
---
+++ db/sql/dropTables.sql
@@ -0,0 +1,5 @@
+-- Atilla Türkmen, Onur Kömürcü
+-- Drops tables for 2. project of CmpE 321 course
+-- Written for MYSQL
+
+DROP TABLE `Database_Manager`, `Grades`, `Prerequisites`, `Course`, `Classroom`, `Student`, `Instructor`, `Department`, `User`;
