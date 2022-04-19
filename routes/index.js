aa9229f16246b938841b2d86348e9ad19cfc94eb
atillaturkmen <turkmenatilla522@gmail.com>
2022-04-19 17:25:48 +0300

    homepage

--- routes/index.js
+++ routes/index.js
@@ -4,17 +4,7 @@ const db = require("../db/db-utils");
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    try {
-        await db.insertDatabaseManager("username", "password");
-    } catch (error) {
-        console.log(error);
-    }
-    try {
-        await db.selectEveryDatabaseManager();
-    } catch (error) {
-        console.log(error);
-    }
-    res.send('Hello World!');
+    res.render("login-selection");
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const port = 80;
 
+app.set("view engine", "ejs");
+
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
 });
