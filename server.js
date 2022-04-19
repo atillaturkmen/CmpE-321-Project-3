1f9ff575fe151aa4442ad00399cefd1a5f1f8235
atillaturkmen <turkmenatilla522@gmail.com>
2022-04-19 14:17:56 +0300

    server works

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+const express = require('express');
+
+const app = express();
+const port = 80;
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
+
+const routes = require("./routes/index");
+app.get("*", routes);
+app.post("*", routes);
